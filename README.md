9a990b87576d9ecce58e59c8f24e8256ae59d57a
HowJMay <vulxj0j8j8@gmail.com>
2021-12-30 16:54:33 +0800

    feat: Implement basic workable example

---
+++ README.md
@@ -0,0 +1,9 @@
+# ark-circom example
+
+This is the minumun example to show to to use ark-circom.
+
+Run with the following command in the project root.
+
+```shell
+$ cargo run
+```
---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use ark_bn254::Bn254;
+use ark_circom::{CircomBuilder, CircomConfig};
+use ark_groth16::{
+    create_random_proof as prove, generate_random_parameters, prepare_verifying_key, verify_proof,
+};
+use ark_std::rand::thread_rng;
+
+fn main() {
+    // Load the WASM and R1CS for witness and proof generation
+    let cfg = CircomConfig::<Bn254>::new("./circom/example-circuit.wasm", "./circom/example-circuit.r1cs").unwrap();
+
+    // Insert our public inputs as key value pairs
+    let mut builder = CircomBuilder::new(cfg);
+    builder.push_input("a", 3);
+    builder.push_input("b", 11);
+
+    // Create an empty instance for setting it up
+    let circom = builder.setup();
+
+    // Run a trusted setup
+    let mut rng = thread_rng();
+    let params = generate_random_parameters::<Bn254, _, _>(circom, &mut rng).unwrap();
+
+    // Get the populated instance of the circuit with the witness
+    let circom = builder.build().unwrap();
+
+    let inputs = circom.get_public_inputs().unwrap();
+
+    // Generate the proof
+    let proof = prove(circom, &params, &mut rng).unwrap();
+
+    // Check that the proof is valid
+    let pvk = prepare_verifying_key(&params.vk);
+    let verified = verify_proof(&pvk, &proof, &inputs).unwrap();
+    assert!(verified);
+
+    print!("verified result: {}\n", verified)
+}
