98439be9cc78eb4c358424584dfd6aebcffc568c
Jhowww <chahsiao@gmail.com>
2022-01-24 14:26:17 +0800

    feat: Add pre-defined string variable of testdata

--- read-from-snarkjs-prove-by-arkworks/src/main.rs
+++ read-from-snarkjs-prove-by-arkworks/src/main.rs
@@ -4,24 +4,19 @@ mod utils {
     pub mod verifier;
     pub mod data_structures;
 }
-
+mod testdata_json;
 
 fn main() {
-    let public_path = Path::new("./src/testdata/build/public.json");
-    let verification_key_path = Path::new("./src/testdata/build/verification_key.json");
-    let proof_path = Path::new("./src/testdata/build/proof.json");
+    let testdata = testdata_json::Json::new();
     
-    let json = std::fs::read_to_string(public_path).unwrap();
-    let json: serde_json::Value = serde_json::from_str(&json).unwrap();
+    let json: serde_json::Value = serde_json::from_str(&testdata.public).unwrap();
     let inputs = utils::loader::load_json_public_input(&json);
     
-    let json = std::fs::read_to_string(verification_key_path).unwrap();
-    let json: serde_json::Value = serde_json::from_str(&json).unwrap();
+    let json: serde_json::Value = serde_json::from_str(&testdata.verification_key).unwrap();
     let vk= utils::loader::load_json_verification_key(&json);
     let pvk = utils::verifier::prepare_verifying_key(&vk);
 
-    let json = ark_std::fs::read_to_string(proof_path).unwrap();
-	let json: serde_json::Value = serde_json::from_str(&json).unwrap();
+	let json: serde_json::Value = serde_json::from_str(&testdata.proof).unwrap();
 	let proof = utils::loader::load_json_proof(&json);
 
     let verified = utils::verifier::verify_proof(&pvk, &proof, &inputs).unwrap();
